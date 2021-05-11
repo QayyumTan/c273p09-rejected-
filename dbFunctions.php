421ded35fa53cc982632f949f1f98c529495bb6f
19041582-Qayyum-Danish-Tan <19041582@myrp.edu.sg>
2021-05-11 21:16:55 +0800

    Update db

--- dbFunctions.php
+++ dbFunctions.php
@@ -30,9 +30,9 @@ foreach ($_SERVER as $key => $value) {
 $link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
 
 if (!$link) {
-    echo "Error: Unable to connect to MySQL.".PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno().PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error().PHP_EOL;
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
     exit;
 }
 ?>
